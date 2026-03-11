72d0486dbba289774cba0556693f4f084743c5e1
NikLiberty <korznikovnikita@gmail.com>
2026-03-11 13:14:52 +0500

    Initial Commit

---
+++ OBD Insight/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OBD Insight
+//
+//  Created by Никита on 11.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OBD Insight/OBD_InsightApp.swift
@@ -0,0 +1,17 @@
+//
+//  OBD_InsightApp.swift
+//  OBD Insight
+//
+//  Created by Никита on 11.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct OBD_InsightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
